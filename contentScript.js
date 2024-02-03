74e43e9cdb99bc39770a8919769573ed2ef18bec
Tyler Tran <tylerduytran@gmail.com>
2024-02-03 12:44:30 -0800

    toggle modularized

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,72 @@
-let blueBlindFilterActive = false;
+// let blueBlindFilterActive = false;
+// let blueWeakFilterActive = false;
+// let RedBlindFilterActive = false;
+// let RedWeakFilterActive = false;
+// let GreenBlindFilterActive = false;
+// let GreenWeakFilterActive = false;
+// let MonochromeFilterActive = false;
+// let BlueConeMonochromacyFilterActive = false;
 
-// adds the blue blind filter to the body class
-function applyBlueBlindFilter() {
+const filters = {
+    'blue-blind': false,
+    'blue-weak': false,
+    'red-blind': false,
+    'red-weak': false,
+    'green-blind': false,
+    'green-weak': false,
+    'monochrome': false,
+    'blue-cone-monochromacy': false
+};
+
+function applyFilter() {
     const body = document.body;
-    if (blueBlindFilterActive) {
-        body.classList.add('blue-blind');
-    } else {
-        body.classList.remove('blue-blind');
+    for (let filter in filters) {
+        if (filters[filter]) {
+            body.classList.add(filter);
+        } else {
+            body.classList.remove(filter);
+        }
     }
 }
 
 browser.runtime.onMessage.addListener(function (message) {
-    if (message.action === 'toggleBlueBlindFilter') {
-        blueBlindFilterActive = !blueBlindFilterActive;
-        applyBlueBlindFilter();
+    // if (message.action in filters) {
+    //     filters[message.action] = !filters[message.action];
+    //     applyFilter();
+    // }
+    if (message.action === 'toggleFilter') {
+    filters['blue-blind'] = !filters['blue-blind'];
+    applyFilter();
     }
 });
 
-applyBlueBlindFilter();
+applyFilter();
+
+// let blueBlindFilterActive = false;
+// function applyBlueBlindFilter() {
+//     const body = document.body;
+//     if (blueBlindFilterActive) {
+//         body.classList.add('blue-blind');
+//     } else {
+//         body.classList.remove('blue-blind');
+//     }
+// }
+
+// browser.runtime.onMessage.addListener(function (message) {
+//     if (message.action === 'toggleBlueBlindFilter') {
+//         blueBlindFilterActive = !blueBlindFilterActive;
+//         applyBlueBlindFilter();
+//     }
+// });
+
+// applyBlueBlindFilter();
+
+
+// browser.runtime.onMessage.addListener(function (message) {
+//     if (message.action === 'toggleBlueBlindFilter') {
+//         blueBlindFilterActive = !blueBlindFilterActive;
+//         applyBlueBlindFilter();
+//     }
+// });
+
+// applyBlueBlindFilter();
--- popup/popup.js
+++ popup/popup.js
@@ -40,9 +40,3 @@ document.addEventListener('DOMContentLoaded', async function() {
     });
 });
 
-
-document.getElementById('toggleButton').addEventListener('click', function () {
-  browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      browser.tabs.sendMessage(tabs[0].id, { action: 'toggleBlueBlindFilter' });
-  });
-});
