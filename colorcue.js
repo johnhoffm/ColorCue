04ec6f81a46e5f7724283d87e93f38a063a5fcee
John Hoffman <johnhoffman@ucla.edu>
2024-02-03 23:13:08 -0800

    daltonizeRGB

--- colorcue.js
+++ colorcue.js
@@ -29,10 +29,11 @@ let daltonizeImage = function (image, options) {
     };
 
     if (!options) options = {};
-    var type = typeof options.type == "string" ? options.type : "Normal",
-        amount = typeof options.amount == "number" ? options.amount : 1.0,
-        canvas = document.createElement("canvas"),
-        ctx = canvas.getContext("2d");
+    var type = typeof options.type == "string" ? options.type : "Normal";
+    // this line is useless
+    // amount = typeof options.amount == "number" ? options.amount : 1.0;
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
     canvas.width = image.width;
     canvas.height = image.height;
     // console.log(image.width);
@@ -108,27 +109,106 @@ let daltonizeImage = function (image, options) {
     }
 };
 
+let daltonizeRGB = function ([red, green, blue], options) {
+    var CVDMatrix = { // Color Vision Deficiency
+        "Protanope": [ // reds are greatly reduced (1% men)
+            0.0, 2.02344, -2.52581,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0
+        ],
+        "Deuteranope": [ // greens are greatly reduced (1% men)
+            1.0, 0.0, 0.0,
+            0.494207, 0.0, 1.24827,
+            0.0, 0.0, 1.0
+        ],
+        "Tritanope": [ // blues are greatly reduced (0.003% population)
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            -0.395913, 0.801109, 0.0
+        ]
+    };
+
+    if (!options) options = {};
+    var type = typeof options.type == "string" ? options.type : "Normal";
+    // this line is useless
+    // var amount = typeof options.amount == "number" ? options.amount : 1.0;
+
+    // Apply Daltonization
+    var cvd = CVDMatrix[type],
+        cvd_a = cvd[0],
+        cvd_b = cvd[1],
+        cvd_c = cvd[2],
+        cvd_d = cvd[3],
+        cvd_e = cvd[4],
+        cvd_f = cvd[5],
+        cvd_g = cvd[6],
+        cvd_h = cvd[7],
+        cvd_i = cvd[8];
+    var L, M, S, l, m, s, R, G, B, RR, GG, BB;
+    data = [red, green, blue];
+    console.log(data);
+        var r = data[0],
+            g = data[1],
+            b = data[2];
+        // RGB to LMS matrix conversion
+        L = (17.8824 * r) + (43.5161 * g) + (4.11935 * b);
+        M = (3.45565 * r) + (27.1554 * g) + (3.86714 * b);
+        S = (0.0299566 * r) + (0.184309 * g) + (1.46709 * b);
+        // Simulate color blindness
+        l = (cvd_a * L) + (cvd_b * M) + (cvd_c * S);
+        m = (cvd_d * L) + (cvd_e * M) + (cvd_f * S);
+        s = (cvd_g * L) + (cvd_h * M) + (cvd_i * S);
+        // LMS to RGB matrix conversion
+        R = (0.0809444479 * l) + (-0.130504409 * m) + (0.116721066 * s);
+        G = (-0.0102485335 * l) + (0.0540193266 * m) + (-0.113614708 * s);
+        B = (-0.000365296938 * l) + (-0.00412161469 * m) + (0.693511405 * s);
+        // Isolate invisible colors to color vision deficiency (calculate error matrix)
+        R = r - R;
+        G = g - G;
+        B = b - B;
+        // Shift colors towards visible spectrum (apply error modifications)
+        RR = (0.0 * R) + (0.0 * G) + (0.0 * B);
+        GG = (0.7 * R) + (1.0 * G) + (0.0 * B);
+        BB = (0.7 * R) + (0.0 * G) + (1.0 * B);
+        // Add compensation to original values
+        R = RR + r;
+        G = GG + g;
+        B = BB + b;
+        // Clamp values
+        if (R < 0) R = 0;
+        if (R > 255) R = 255;
+        if (G < 0) G = 0;
+        if (G > 255) G = 255;
+        if (B < 0) B = 0;
+        if (B > 255) B = 255;
+        // Record color
+        data[0] = R >> 0;
+        data[1] = G >> 0;
+        data[2] = B >> 0;
+        return data
+};
+
 // ==== recursively adjust colors on document.body
-function adjustColors(element) {
+function adjustColors(element, options) {
     // Recursively adjust colors on all child nodes of the given element.
     if (element.childNodes.length) {
         element.childNodes.forEach(function (child) {
-            adjustColors(child);
+            adjustColors(child, options);
         });
     }
 
     if (element.nodeType === Node.ELEMENT_NODE) {
         // Adjust text color
         const textColor = window.getComputedStyle(element).color;
-        element.style.color = adjustSingleColor(textColor);
+        element.style.color = adjustSingleColor(textColor, options);
 
         // Adjust background color
         const backgroundColor = window.getComputedStyle(element).backgroundColor;
-        element.style.backgroundColor = adjustSingleColor(backgroundColor);
+        element.style.backgroundColor = adjustSingleColor(backgroundColor, options);
 
         // Adjust border color
         const borderColor = window.getComputedStyle(element).borderColor;
-        element.style.borderColor = adjustSingleColor(borderColor);
+        element.style.borderColor = adjustSingleColor(borderColor, options);
 
         // Adjust image colors
         if (element.tagName === "IMG") {
@@ -163,7 +243,7 @@ function adjustColors(element) {
     }
 }
 
-function adjustSingleColor(input) {
+function adjustSingleColor(input, options) {
     const rgbRegex = /rgb\((\s*\d+\s*,\s*\d+\s*,\s*\d+\s*)\)/g;
     const rgbaRegex = /rgba\((\s*\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*\d+(\.\d+)?\s*)\)/g;
 
@@ -179,8 +259,8 @@ function adjustSingleColor(input) {
             }
             );
             // daltonize single color
-            // const resultArray = daltonizeColor(valuesArray);
-            return `rgb(${valuesArray.join(', ')})`;
+            const resultArray = daltonizeRGB(valuesArray, options);
+            return `rgb(${resultArray.join(', ')})`;
         });
     } else if (rgbaRegex.test(input)) {
         resultString = input.replace(rgbaRegex, (match) => {
@@ -193,8 +273,8 @@ function adjustSingleColor(input) {
             }
             );
             // daltonize single color
-            // const resultArray = daltonizeColor(valuesArray.slice(0, 3));
-            return `rgba(${valuesArray.join(', ')}, ${valuesArray[3]})`;
+            const resultArray = daltonizeRGB(valuesArray.slice(0, 3), options);
+            return `rgba(${resultArray.join(', ')}, ${valuesArray[3]})`;
         });
     } else {
         console.log(`invalid color type: ${input}`)
@@ -206,9 +286,11 @@ function adjustSingleColor(input) {
 browser.runtime.onMessage.addListener(async (request) => {
     let enabled = (await browser.storage.local.get()).enabled
     let images = (await browser.storage.local.get()).images
+    // TODO: hardcoded options for now, change to pull from local storage
+    options = {type: "Deuteranope"};
     if (enabled) {
         // TODO: turn on filter for only text and colors
-        adjustColors(document.body);
+        adjustColors(document.body, options);
         if (images) {
             // TODO: turn on filter for only images
         }
@@ -221,9 +303,11 @@ browser.runtime.onMessage.addListener(async (request) => {
 async function init() {
     let enabled = (await browser.storage.local.get()).enabled
     let images = (await browser.storage.local.get()).images
+    // TODO: options are hardcoded, change later
+    options = {type: "Deuteranope"};
     if (enabled) {
         // TODO: turn on filter for only text and colors
-        adjustColors(document.body);
+        adjustColors(document.body, options);
         if (images) {
             // TODO: turn on filter for only images
         }
