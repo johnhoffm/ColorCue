d91fe0ad07d8b2cd4a7f964f814239b1a1214b22
John Hoffman <johnhoffman@ucla.edu>
2024-07-29 00:42:54 -0600

    Refactor daltonizeImage to use daltonizeRGB

--- colorcue.js
+++ colorcue.js
@@ -14,29 +14,8 @@
 */
 
 let daltonizeImage = function (image, options) {
-    var CVDMatrix = { // Color Vision Deficiency
-        "Protanopia": [ // reds are greatly reduced (1% men)
-            0.0, 2.02344, -2.52581,
-            0.0, 1.0, 0.0,
-            0.0, 0.0, 1.0
-        ],
-        "Deuteranopia": [ // greens are greatly reduced (1% men)
-            1.0, 0.0, 0.0,
-            0.494207, 0.0, 1.24827,
-            0.0, 0.0, 1.0
-        ],
-        "Tritanopia": [ // blues are greatly reduced (0.003% population)
-            1.0, 0.0, 0.0,
-            0.0, 1.0, 0.0,
-            -0.395913, 0.801109, 0.0
-        ]
-    };
-
-    if (!options) options = {};
     if (image.width === 0 || image.height === 0) return;
-    var type = typeof options.type == "string" ? options.type : "Normal";
-    // this line is useless
-    // amount = typeof options.amount == "number" ? options.amount : 1.0;
+
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
     canvas.width = image.width;
@@ -47,61 +26,22 @@ let daltonizeImage = function (image, options) {
     ctx.drawImage(image, 0, 0, image.width, image.height);
     try {
         var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        var data = imageData.data;
+        var rawData = imageData.data;
     } catch (e) {
-        console.error(e);
+        console.error("Unable to access image data with error: " + e);
     }
-    // Apply Daltonization
-    var cvd = CVDMatrix[type],
-        cvd_a = cvd[0],
-        cvd_b = cvd[1],
-        cvd_c = cvd[2],
-        cvd_d = cvd[3],
-        cvd_e = cvd[4],
-        cvd_f = cvd[5],
-        cvd_g = cvd[6],
-        cvd_h = cvd[7],
-        cvd_i = cvd[8];
-    var L, M, S, l, m, s, R, G, B, RR, GG, BB;
-    for (var id = 0, length = data.length; id < length; id += 4) {
-        var r = data[id],
-            g = data[id + 1],
-            b = data[id + 2];
-        // RGB to LMS matrix conversion
-        L = (17.8824 * r) + (43.5161 * g) + (4.11935 * b);
-        M = (3.45565 * r) + (27.1554 * g) + (3.86714 * b);
-        S = (0.0299566 * r) + (0.184309 * g) + (1.46709 * b);
-        // Simulate color blindness
-        l = (cvd_a * L) + (cvd_b * M) + (cvd_c * S);
-        m = (cvd_d * L) + (cvd_e * M) + (cvd_f * S);
-        s = (cvd_g * L) + (cvd_h * M) + (cvd_i * S);
-        // LMS to RGB matrix conversion
-        R = (0.0809444479 * l) + (-0.130504409 * m) + (0.116721066 * s);
-        G = (-0.0102485335 * l) + (0.0540193266 * m) + (-0.113614708 * s);
-        B = (-0.000365296938 * l) + (-0.00412161469 * m) + (0.693511405 * s);
-        // Isolate invisible colors to color vision deficiency (calculate error matrix)
-        R = r - R;
-        G = g - G;
-        B = b - B;
-        // Shift colors towards visible spectrum (apply error modifications)
-        RR = (0.0 * R) + (0.0 * G) + (0.0 * B);
-        GG = (0.7 * R) + (1.0 * G) + (0.0 * B);
-        BB = (0.7 * R) + (0.0 * G) + (1.0 * B);
-        // Add compensation to original values
-        R = RR + r;
-        G = GG + g;
-        B = BB + b;
-        // Clamp values
-        if (R < 0) R = 0;
-        if (R > 255) R = 255;
-        if (G < 0) G = 0;
-        if (G > 255) G = 255;
-        if (B < 0) B = 0;
-        if (B > 255) B = 255;
-        // Record color
-        data[id] = R >> 0;
-        data[id + 1] = G >> 0;
-        data[id + 2] = B >> 0;
+
+    // Iterate through each pixel's RGBA values
+    // R, G, B, A are stored in 4 consecutive array indices, hence the 4 step size
+    for (var id = 0, length = rawData.length; id < length; id += 4) {
+        let [r, g, b, a] = rawData.slice(id, id + 4);
+
+        let data = daltonizeRGB([r, g, b], options);
+
+        rawData[id] = data[0];
+        rawData[id + 1] = data[1];
+        rawData[id + 2] = data[2];
+        // Do nothing with alpha value
     }
     // Record data
     ctx.putImageData(imageData, 0, 0);
@@ -111,7 +51,10 @@ let daltonizeImage = function (image, options) {
 };
 
 let daltonizeRGB = function ([red, green, blue], options) {
-    var CVDMatrix = { // Color Vision Deficiency
+    if (!options) options = {};
+    var type = typeof options.type == "string" ? options.type : "Normal";
+
+    var CVDMatrix = {
         "Protanopia": [ // reds are greatly reduced (1% men)
             0.0, 2.02344, -2.52581,
             0.0, 1.0, 0.0,
@@ -128,11 +71,6 @@ let daltonizeRGB = function ([red, green, blue], options) {
             -0.395913, 0.801109, 0.0
         ]
     };
-
-    if (!options) options = {};
-    var type = typeof options.type == "string" ? options.type : "Normal";
-    // this line is useless
-    // var amount = typeof options.amount == "number" ? options.amount : 1.0;
 
     // Apply Daltonization
     var cvd = CVDMatrix[type],
